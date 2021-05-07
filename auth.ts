b694211d8ad9b905fd57842cfac50051a98191f7
Robin Heidenis <robin.heidenis@gmail.com>
2021-05-07 18:10:24 +0200

    added authentication server logic

---
+++ auth.ts
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const users = [
+    {
+        username: process.env.username,
+        password: process.env.password, // todo pls change this omg
+        role: "user"
+    }
+];
+
+const accessTokenSecret = process.env.accessTokenSecret;
+const refreshTokenSecret = process.env.refreshTokenSecret;
+let refreshTokens = [];
+
+app.post("/login", (req, res) => {
+    // read username and password from request body
+    const { username, password } = req.body;
+
+    // filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password; });
+
+    if (user) {
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: "60m" });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
+
+        res.json({
+            accessToken,
+            refreshToken
+        });
+    } else {
+        res.send("Username or password incorrect");
+    }
+});
+
+app.post("/token", (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: "20m" });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+app.post("/logout", (req, res) => {
+    const { token } = req.body;
+    refreshTokens = refreshTokens.filter(t => t !== token);
+
+    res.send("Logout successful");
+});
+
+app.listen(3002, () => {
+    console.log("Authentication service started on port 3002");
+});
